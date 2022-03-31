5adfbcfef556b654db1614414a3910cdd8bca70b
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2022-03-31 13:34:34 +0200

    character set replaced

--- gen_pasw.py
+++ gen_pasw.py
@@ -1,11 +1,8 @@
 import random
+import string
 
-low = 'qwertyuiopasdfghjklzxcvbnm'
-big = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-num = '1234567890'
-spec = '!@#$%^&amp;*()'
+all = string.ascii_letters + string.digits + string.punctuation
 
-all = low + big + num + spec
 password_len = int(input('Długość hasla:  '))
 password = "".join(random.sample(all, password_len))
 print(password)
